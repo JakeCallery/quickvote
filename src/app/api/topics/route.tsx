52842848c8ffe8d77fdba399c4a3ddea4b9fe57c
Jake <jake@subvoicestudios.com>
2023-10-26 16:41:19 -0500

    starting create topic functionality

--- src/app/api/topics/route.tsx
+++ src/app/api/topics/route.tsx
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/db";
 import schema from "@/app/api/topics/schema";
+import { getServerSession } from "next-auth";
 export async function GET(req: NextRequest) {
   const topics = await prisma.topic.findMany();
   return NextResponse.json(topics);
 }
 
 export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+  console.log("Session: ", session);
   const body = await req.json();
   const validation = schema.safeParse(body);
 
